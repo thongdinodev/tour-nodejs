9eabb232505db83975f59fbe7aee93e33742ff50
Thong Dino <hodinhthong.auto@gmail.com>
2024-03-19 11:05:11 +0700

    build calcAverageRating to update, delete review features

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -29,11 +29,15 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
+    // this.populate({
+    //     path: 'tour',
+    //     select: 'name duration price '
+    // })
+    
     this.populate({
-        path: 'tour',
-        select: 'name duration price '
-    }).populate({
         path: 'user',
         select: 'name email'
     });
@@ -41,30 +45,56 @@ reviewSchema.pre(/^find/, function (next) {
     next();
 });
 
-// static point at MODEL, instance method point at DOCUMENT
-reviewSchema.statics.calcAvgRatings = async function(tourId) {
+// STATIC IS POINT TO CURRENT MODEL, INSTANCE METHOD POINT TO DOCUMENT
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
     const stats = await this.aggregate([
         {
-            $match: { tour: tourId}
+            $match: { tour: tourId }
         },
         {
             $group: {
                 _id: '$tour',
                 nRating: { $sum: 1 },
-                avgRating: { $avg: '$rating' }
+                avgRating: { $avg: '$rating'}
             }
         }
     ]);
     console.log(stats);
-    // update document
-    await Tour.findByIdAndUpdate(tourId, {
-        ratingsQuantity: stats[0].nRating,
-        ratingsAverage: stats[0].avgRating
-    });
+
+    //update tour rating
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        // avoid delete all reviews, set review as default
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
 };
 
-reviewSchema.post('save', function() {
-    this.constructor.calcAvgRatings(this.tour)
+reviewSchema.post('save', function () {
+    // to point at the current MODEL
+    // this.tour is an inObject
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+// for findByIdAndUpdate and findByIdAndDelete update data when update, delete
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+    this.r = await this.clone().findOne();
+    console.log(this.r);
+    next();
+  });
+  
+reviewSchema.post(/^findOneAnd/, async function() {
+    // await this.findOne(); does NOT work here, query has already executed
+    // this.r.tour must be tourId
+
+    await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
