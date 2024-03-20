21185e06c83cec0cd8200cde46ef46c05decf718
Thong Dino <hodinhthong.auto@gmail.com>
2024-03-20 21:02:21 +0700

    build overview.ejs to render data (all tours)

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,9 +1,17 @@
-exports.getOverview = (req, res, next) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appErrors');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+    const tours = await Tour.find();
+
+
     res.status(200).render('overview', {
         title: 'All tour Page',
-        tour: 'This is all tour page'
+        tour: 'This is all tour page NODEJS Project',
+        tours
     });
-};
+});
 
 exports.getTour = (req, res, next) => {
     res.status(200).render('tour', {
