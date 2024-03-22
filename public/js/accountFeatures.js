b0646f8fa49dd377ed58970554f4bb3d7b8701bf
Thong Dino <hodinhthong.auto@gmail.com>
2024-03-22 21:39:24 +0700

    decorate alert, assign to "/" route when login success

---
+++ public/js/accountFeatures.js
@@ -0,0 +1,58 @@
+// axios are promise, so use async, await
+// create a cookie for client side to login account
+
+// DOM ELEMENTS
+const loginForm = document.querySelector('.form');
+const logoutBtn = document.querySelector('.logout_btn');
+
+// show ALERT function
+const hideAlert = () => {
+    const el = document.querySelector('.alert');
+    if (el) el.parentElement.removeChild(el);
+};
+
+const showAlert = (type, msg) => {
+    hideAlert();
+    const markup = `<div class="alert alert--${type}">${msg}</div>`;
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+    window.setTimeout(hideAlert, 5000);
+};
+
+// LOGIN feature
+const loginUser = async (email, password) => {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: '/api/users/login',
+            data: {
+                email,
+                password
+            }  
+        });
+        console.log(res);
+
+        if (res.data.status === 'success') {
+            showAlert('success', 'Logged in successfully');
+            window.setTimeout(() => {
+                location.assign('/');
+            }, 1500);
+        }
+    } catch (error) {
+        showAlert('error', error.response.data.message);
+    }
+};
+
+
+
+// CONDITION TO EXCUTE CODE
+if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    
+        // get data from client
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        
+        loginUser(email, password);
+    });
+}
