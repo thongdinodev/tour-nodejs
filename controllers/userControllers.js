be26f2619ee7b8e97cbf06a7755c1463b22c9e0f
Thong Dino <hodinhthong.auto@gmail.com>
2024-03-24 20:11:43 +0700

    use sharp to resize image

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 
 const User = require('../models/userModel');
 const AppError = require('../utils/appErrors');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('../controllers/handlerFactory');
 
-const multerStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/img/users/');
-    },
-    filename: function (req, file, cb) {
-        const ext = `${file.mimetype.split('/')[1]}`;
-        cb(null, `user-${req.user._id}-${Date.now()}.${ext}`);
-    }
-});
+// const multerStorage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'public/img/users/');
+//     },
+//     filename: function (req, file, cb) {
+//         const ext = `${file.mimetype.split('/')[1]}`;
+//         cb(null, `user-${req.user._id}-${Date.now()}.${ext}`);
+//     }
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
@@ -50,9 +53,9 @@ exports.getMe = (req, res, next) => {
 
 exports.uploadUserPhoto = upload.single('photo');
 
+
 exports.updateMe = catchAsync(async (req, res, next) => {
     console.log(req.file);
-    console.log(req.body);
     // 1) if create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
         return next(new AppError('This route is not for update password, please use /updateMyPassword', 400));
@@ -61,8 +64,23 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     // 2) Update user document
     // can't use save, create because password and confirmpassword is required
     const filteredBody = filterObj(req.body, 'name', 'email');
-    if (req.file.filename) filteredBody.photo = req.file.filename; // update photo field(model)
-    // filteredBody = {name, email, photo: ...};
+    if (req.file) {
+        req.file.filename = `user-${req.user._id}-${Date.now()}.jpeg`;
+
+        if (req.file.filename) filteredBody.photo = req.file.filename; // update photo field(model)
+        // filteredBody = {name, email, photo: ...};
+        await sharp(req.file.buffer)
+                .resize(500, 500)
+                .toFormat('jpeg')
+                .jpeg({ quality: 90 })
+                .toFile(`public/img/users/${req.file.filename}`, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('image resized');
+                    }
+            });
+    }
 
     const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
         new: true,
